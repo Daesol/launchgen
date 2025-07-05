fa4c36d0f6af73b7abdc7e6b745d9802a3880965
Daesol <daesol97@gmail.com>
2025-07-05 05:31:08 -0400

    mobile update

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -42,7 +42,7 @@ export function HeroSection({ email, setEmail, isSubmitted, isSubmitting, isWait
       <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-purple-500/20 rounded-full blur-3xl animate-pulse"></div>
       <div className="absolute bottom-1/4 right-1/4 w-80 h-80 bg-pink-500/20 rounded-full blur-3xl animate-pulse [animation-delay:1s]"></div>
 
-      <div className="relative min-h-screen flex items-center">
+      <div className="relative min-h-screen flex items-center pt-16">
         <div className="container mx-auto px-4 py-8 lg:py-0">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center">
             {/* Left Column - Content */}
